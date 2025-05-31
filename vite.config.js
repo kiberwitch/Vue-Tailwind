324423f7753092b1dc56a321476e3bdbbd2c648f
kiberwitch <fokd7446@gmail.com>
2025-05-31 13:28:08 +0300

    добавлениеgh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
-    tailwindcss(),
+    VueDevTools()
   ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer()
+      ]
+    }
+  },
+  base: '/Vue-Tailwindd/', // Используйте base вместо publicPath
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
 })
